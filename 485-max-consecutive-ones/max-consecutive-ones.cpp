b1eafefdf5b8774290ad3e68d40351cb22be943b
Siddhartha Singh <siddharthasingh3112004@gmail.com>
2026-04-02 19:36:23 +0530

    Time: 0 ms (100.00%) | Memory: 50.3 MB (5.32%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -4,7 +4,7 @@ public:
         int count  = 0;
         int maxi = 0;
         for(int i = 0 ; i < nums.size(); i++) {
-            if(nums[i] ==  1 ) {
+            if(nums[i]  != 0 ) {
                 count++;
                 maxi = max(maxi, count);
             } else {
