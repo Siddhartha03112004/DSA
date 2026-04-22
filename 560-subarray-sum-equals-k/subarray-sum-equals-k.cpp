8944c953c73c6d322af0f89b6371f07d253c8e89
Siddhartha Singh <siddharthasingh3112004@gmail.com>
2026-04-22 08:56:19 +0530

    Time: 63 ms (31.99%) | Memory: 47.6 MB (26.49%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
+        map<int, int> mpp;
+
+        mpp[0] = 1;
+        int preSum = 0;
         int count = 0;
 
-        for (int i = 0; i < n; i++) {
-            int sum = 0;  // restart karo sum for each i
-            for (int j = i; j < n; j++) {
-                sum = sum +  nums[j];   // add elements one by one
-                if (sum == k) {
-                    count++;
-                }
+        for(int i = 0; i < nums.size(); i++) {
+            preSum += nums[i];
+
+            int remove = preSum - k;
+
+            if(mpp.find(remove) != mpp.end()) {
+                count += mpp[remove];
             }
+
+            mpp[preSum] += 1;
         }
 
         return count;
