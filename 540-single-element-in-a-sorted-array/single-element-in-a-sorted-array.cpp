51287d80432d2d792d9bf04c0eeb163a9343c9f8
Siddhartha Singh <siddharthasingh3112004@gmail.com>
2026-05-09 09:54:24 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (67.81%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,15 +1,49 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int, int> mpp;
-        for (int num : nums) {
-            mpp[num]++;
-        }
-        for (auto &x : mpp) {
-            if (x.second == 1) {
-                return x.first; // return the element that appears once
+
+        int n = nums.size();
+
+        // if only one element exists
+        if (n == 1)
+            return nums[0];
+
+        // check first element
+        if (nums[0] != nums[1])
+            return nums[0];
+
+        // check last element
+        if (nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
+
+        int low = 1;
+        int high = n - 2;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            // element found
+            if ((nums[mid - 1] != nums[mid]) &&
+                (nums[mid] != nums[mid + 1])) {
+
+                return nums[mid];
+            }
+            // checking using index  (odd, even )
+            // we are on left side   checking left first   
+            if ((mid % 2 == 1 && nums[mid - 1] == nums[mid]) ||
+                (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+
+                low = low + 1;
+            }
+
+            // we are on right side
+            else {
+
+                high = high - 1;
             }
         }
-        return -1; // should never reach here
+
+        return -1;
     }
 };
