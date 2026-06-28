55b68bd92a15abaf4aa782092645392f883e1c47
Siddhartha Singh <siddharthasingh3112004@gmail.com>
2026-06-28 09:57:51 +0530

    Time: 3 ms (45.15%) | Memory: 11 MB (8.12%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    bool check(int left , int right , string& x) {
+        if(left >= right) {
+            return true;
+        }
+        if(x[left] != x[right]) {
+            return false;
+        }
+         return check (left+1, right -1 , x);
+
+    }
+
+
+public:
+    bool isPalindrome(int x) {
+            if (x < 0)
+            return false;
+
+            string s = to_string(x);
+            return check(0, s.size() - 1, s);    
+    }
+};
