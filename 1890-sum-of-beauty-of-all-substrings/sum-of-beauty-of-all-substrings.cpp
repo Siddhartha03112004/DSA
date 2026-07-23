4895793e32b7f4d582084b66798542d2d6a2e6b0
Siddhartha Singh <siddharthasingh3112004@gmail.com>
2026-07-23 10:10:13 +0530

    Time: 123 ms (34.30%) | Memory: 26.1 MB (23.56%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int ans  =  0 ; 
+        int n = s.size();
+
+        for(int i  = 0   ; i < n ; i++) {
+            unordered_map<char, int> mpp;
+
+            for(int j = i ; j < n; j++) {
+                mpp[s[j]]++;
+
+                int maxi = 0;
+                int mini = INT_MAX;
+
+                for(auto it : mpp) {
+                    maxi = max(maxi, it.second);
+                    mini = min(mini, it.second);
+                }
+                
+                ans += (maxi - mini);
+            }
+        }
+        return ans;
+    }
+};
