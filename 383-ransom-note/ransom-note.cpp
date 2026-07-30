2d4e4c2c35e5af6745cd87ad092a6b1d76c20db4
Siddhartha Singh <siddharthasingh3112004@gmail.com>
2026-07-30 09:33:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (89.65%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        int n  = ransomNote.size();
-        int m  = magazine.size();
 
-        unordered_map<char, int> mpr;
-        unordered_map<char, int> mpm;
+        int freq[26] = {0};
 
+        for (char ch : magazine) {
+            freq[ch - 'a']++;
+        }
         for(char ch : ransomNote) {
-            mpr[ch]++;
-        }
-        for(char ch : magazine) {
-            mpm[ch]++;
+            freq[ch - 'a']--;
         }
 
-       for (auto it : mpr) {
-            char ch = it.first;
-            int freq = it.second;
-
-            if(mpm[ch] < freq) {
+        for(int i  = 0 ; i < 26; i++) {
+            if(freq[i] < 0) {
                 return false;
             }
-       }
-       return true;
-       
+        }
+        
+        return true;
     }
 };
