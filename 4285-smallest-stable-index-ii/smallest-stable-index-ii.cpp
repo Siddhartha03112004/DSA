5899a7f0a20ff67e98c5297b55151fb17ef44d34
Siddhartha Singh <siddharthasingh3112004@gmail.com>
2026-09-09 07:13:30 +0530

    Time: 8 ms (59.70%) | Memory: 202.6 MB (71.25%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -2,22 +2,35 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        int maxi = INT_MIN;
-        
+
+        // suffixMin[i] = minimum element from index i to n-1
         vector<int> suffixMin(n);
 
-        suffixMin[n-1] = nums[n - 1];
+        // For the last index, suffix contains only nums[n-1]
+        suffixMin[n - 1] = nums[n - 1];
 
-        for(int i =  n-2  ; i  >= 0 ; i--) {
-           suffixMin[i] = min(nums[i], suffixMin[i + 1]);
+        // Build suffix minimum from right to left
+        for(int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = min(nums[i], suffixMin[i + 1]);
         }
 
-        for(int i  =  0 ; i  < n; i++ ) {
-            maxi = max ( maxi, nums[i]);
-        if(maxi - suffixMin[i] <= k) {
-            return i;
+        // maxi = maximum element from index 0 to i
+        int maxi = INT_MIN;
+
+        // Check every index from left to right
+        for(int i = 0; i < n; i++) {
+
+            // Update maximum of nums[0...i]
+            maxi = max(maxi, nums[i]);
+
+            // Instability score:
+            // max(nums[0...i]) - min(nums[i...n-1])
+            if(maxi - suffixMin[i] <= k) {
+                return i;   // first stable index
+            }
         }
-        }   
+
+        // No stable index found
         return -1;
     }
 };
